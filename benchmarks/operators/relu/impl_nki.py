a14238ce52d60022de179102eaeb5c4aed1a2d3c
Ubuntu <ubuntu@ip-172-31-36-205.ap-southeast-4.compute.internal>
2026-06-10 19:05:31 +0000

    Initial commit for nki relu kernel

---
+++ benchmarks/operators/relu/impl_nki.py
@@ -0,0 +1,38 @@
+import torch
+from torch_xla.core import xla_model as xm
+
+try:
+    import neuronxcc.nki as nki
+    import neuronxcc.nki.language as nl
+    import neuronxcc.nki.isa as nisa
+    PMAX = nl.tile_size.pmax
+except ImportError:
+    nki = None
+
+if nki is not None:
+    @nki.jit
+    def relu_kernel(a_input):
+        num_blocks = (a_input.shape[0] + (PMAX - 1)) // PMAX
+
+        hbm_result_tile = nl.ndarray(a_input.shape, dtype=a_input.dtype, buffer=nl.hbm)
+
+        for i in range(num_blocks):
+            offset = i*PMAX
+
+            partition_index = nl.arange(PMAX)[:, None]
+            free_dim_index = nl.arange(a_input.shape[1])[None, :]
+            
+            mask = partition_index < (a_input.shape[0] - offset)
+
+            a_tile = nl.load(a_input[offset + partition_index, free_dim_index], mask=mask)
+
+            result_tile = nl.maximum(a_tile, 0, mask=mask)
+            
+            nl.store(hbm_result_tile[offset + partition_index, free_dim_index], value=result_tile, mask=mask)
+
+        return hbm_result_tile
+
+def run(x: torch.Tensor, block_size: int = 1024, autotune=False, **kwargs) -> torch.Tensor:
+    x_2d = x.reshape(-1, 1)
+    result = relu_kernel(x_2d)
+    return result.reshape(-1)
