e9323636cb2efe37a9de858e5e44a97e14143fde
Ubuntu <ubuntu@ip-172-31-36-205.ap-southeast-4.compute.internal>
2026-06-16 21:47:23 +0000

    Removed unnecessary imports, switched to free-dimension tiling

--- benchmarks/operators/relu/impl_nki.py
+++ benchmarks/operators/relu/impl_nki.py
@@ -1,38 +1,52 @@
 import torch
-from torch_xla.core import xla_model as xm
 
 try:
     import neuronxcc.nki as nki
     import neuronxcc.nki.language as nl
     import neuronxcc.nki.isa as nisa
     PMAX = nl.tile_size.pmax
+    FMAX_SBUF = 64000
 except ImportError:
     nki = None
 
 if nki is not None:
     @nki.jit
     def relu_kernel(a_input):
-        num_blocks = (a_input.shape[0] + (PMAX - 1)) // PMAX
+        free_dim = min(a_input.shape[1], FMAX_SBUF)
+
+        num_free_blocks = (a_input.shape[1] + (free_dim - 1)) // free_dim
 
         hbm_result_tile = nl.ndarray(a_input.shape, dtype=a_input.dtype, buffer=nl.hbm)
 
-        for i in range(num_blocks):
-            offset = i*PMAX
+        partition_index = nl.arange(PMAX)[:, None]
 
-            partition_index = nl.arange(PMAX)[:, None]
-            free_dim_index = nl.arange(a_input.shape[1])[None, :]
+        for j in range(num_blocks):
+            free_offset = j * free_dim
+
+            free_dim_index = nl.arange(free_dim)[None, :]
             
-            mask = partition_index < (a_input.shape[0] - offset)
+            free_mask = free_dim_index < (a_input.shape[1] - free_offset)
 
-            a_tile = nl.load(a_input[offset + partition_index, free_dim_index], mask=mask)
+            a_tile = nl.load(a_input[partition_index, free_offset + free_dim_index], mask=free_mask)
 
-            result_tile = nl.maximum(a_tile, 0, mask=mask)
+            result_tile = nl.maximum(a_tile, 0, mask=free_mask)
             
-            nl.store(hbm_result_tile[offset + partition_index, free_dim_index], value=result_tile, mask=mask)
+            nl.store(hbm_result_tile[partition_index, free_offset + free_dim_index], value=result_tile, mask=free_mask)
 
         return hbm_result_tile
 
 def run(x: torch.Tensor, block_size: int = 1024, autotune=False, **kwargs) -> torch.Tensor:
-    x_2d = x.reshape(-1, 1)
+    n = x.numel()
+    free_dim = (n + (PMAX - 1)) // PMAX
+    padded_size = PMAX * free_dim
+
+    if padded_size > n:
+        x = torch.nn.functional.pad(x, (0, padded_size - n))
+
+    x_2d = x.reshape(PMAX, free_dim)
     result = relu_kernel(x_2d)
-    return result.reshape(-1)
+
+    return result.reshape(-1)[:n]
+
+def get_last_config() -> dict | None:
+    return None
