1e53253d54165e59b5f7d9885cc450f4171ace01
Ubuntu <ubuntu@ip-172-31-36-205.ap-southeast-4.compute.internal>
2026-06-16 18:49:59 +0000

    Added nki swiglu kernel

---
+++ benchmarks/operators/swiglu/impl_nki.py
@@ -0,0 +1,42 @@
+import torch
+from torch_xla.core import xla_model as xm
+
+try:
+    import neuronxcc.nki as nki
+    import neuronxcc.nki.language as nl
+    import neuronxcc.nki.isa as nisa
+    PMAX = nl.tile_size.pmax
+except ImportError:
+    nki = None
+
+if nki is not None:
+    @nki.jit
+    def swiglu_kernel(x_input, y_input):
+        num_blocks = (x_input.shape[0] + PMAX - 1) // PMAX
+
+        hbm_result_tile = nl.ndarray(x_input.shape, dtype=x_input.dtype, buffer=nl.hbm)
+
+        for i in range(num_blocks):
+            offset = i*PMAX
+
+            partition_index = nl.arange(PMAX)[:, None]
+            free_dim_index = nl.arange(x_input.shape[1])[None, :]
+
+            mask = partition_index < (x_input.shape[0] - offset)
+
+            x_tile = nl.load(x_input[offset + partition_index, free_dim_index], mask=mask)
+            y_tile = nl.load(y_input[offset + partition_index, free_dim_index], mask=mask)
+
+            silu_x = nl.silu(x_tile, mask=mask)
+            result_tile = nl.multiply(silu_x, y_tile, mask=mask)
+
+            nl.store(hbm_result_tile[offset + partition_index, free_dim_index], value=result_tile, mask=mask)
+        
+        return hbm_result_tile
+
+def run(x: torch.Tensor, y: torch.Tensor, block_size: int = 1024, autotune: bool = False, **kwargs) -> torch.Tensor:
+    return swiglu_kernel(x, y)
+
+
+def get_last_config() -> dict | None:
+    return None
