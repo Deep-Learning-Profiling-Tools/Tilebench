5825a8bb3734e6339b75de6406c390d816b7e280
Ubuntu <ubuntu@ip-172-31-36-205.ap-southeast-4.compute.internal>
2026-06-16 23:09:27 +0000

    minor changes

--- benchmarks/operators/reverse_array/impl_nki.py
+++ benchmarks/operators/reverse_array/impl_nki.py
@@ -1,5 +1,4 @@
 import torch
-from torch_xla.core import xla_model as xm
 
 try:
     import neuronxcc.nki as nki
@@ -36,3 +35,6 @@ def run(x: torch.Tensor, n: int, block_size: int = 1024, autotune=False, **kwarg
     x_2d = x.reshape(-1, 1)
     result = reverse_kernel(x_2d)
     return result.reshape(-1)
+
+def get_last_config() -> dict | None:
+    return None
