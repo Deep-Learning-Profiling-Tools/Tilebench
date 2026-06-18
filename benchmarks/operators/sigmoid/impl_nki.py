5845b3536079a7f0833590910c90c8476cc6e973
Ubuntu <ubuntu@ip-172-31-36-205.ap-southeast-4.compute.internal>
2026-06-18 22:02:02 +0000

    fixed run() signature

--- benchmarks/operators/sigmoid/impl_nki.py
+++ benchmarks/operators/sigmoid/impl_nki.py
@@ -31,7 +31,7 @@ if nki is not None:
 
         return hbm_result_tile
 
-def run(x: torch.Tensor, block_size: int = 1024, autotune=False, **kwargs) -> torch.Tensor:
+def run(x: torch.Tensor, N: int, block_size: int = 1024, autotune=False, **kwargs) -> torch.Tensor:
     if x.dtype == torch.int8:
         raise NotImplementedError("sigmoid NKI: int8 not supported")
     
