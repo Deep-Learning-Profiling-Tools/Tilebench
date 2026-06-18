d42b117cea6e09b3121f8e7f7a873beffccd393d
Ubuntu <ubuntu@ip-172-31-36-205.ap-southeast-4.compute.internal>
2026-06-18 16:14:01 +0000

    fixed tiling logic

--- benchmarks/operators/reverse_array/impl_nki.py
+++ benchmarks/operators/reverse_array/impl_nki.py
@@ -18,7 +18,7 @@ if nki is not None:
         hbm_result_tile = nl.ndarray(a_input.shape, dtype=a_input.dtype, buffer=nl.hbm)
 
         for i in range(num_blocks):
-            offset = i*PMAX
+            offset = i * PMAX
 
             partition_index = nl.arange(PMAX)[:, None]
             free_dim_index = nl.arange(a_input.shape[1])[None, :]
@@ -26,15 +26,27 @@ if nki is not None:
             mask = partition_index < (n - offset)
 
             a_tile = nl.load(a_input[offset + partition_index, free_dim_index], mask=mask)
+
+            rev_partition = PMAX - 1 - partition_index
+            rev_free = a_input.shape[1] - 1 - free_dim_index
             
-            nl.store(hbm_result_tile[n - 1 - (offset + partition_index), free_dim_index], value=a_tile, mask=mask)
+            nl.store(hbm_result_tile[rev_partition, rev_free], value=a_tile, mask=mask)
 
         return hbm_result_tile
 
 def run(x: torch.Tensor, n: int, block_size: int = 1024, autotune=False, **kwargs) -> torch.Tensor:
-    x_2d = x.reshape(-1, 1)
+    free_dim = (n + (PMAX - 1)) // PMAX
+    padded_size = PMAX * free_dim
+
+    if padded_size > n:
+        x = torch.nn.functional.pad(x, (0, padded_size - n))
+
+    x_2d = x.reshape(PMAX, free_dim)
     result = reverse_kernel(x_2d)
-    return result.reshape(-1)
+
+    pad_count = padded_size - n
+
+    return result.reshape(-1)[pad_count:]
 
 def get_last_config() -> dict | None:
     return None
