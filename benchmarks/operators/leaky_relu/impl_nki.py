04839d99eae770fc9b3a014780431e1e154a4d18
Ubuntu <ubuntu@ip-172-31-36-205.ap-southeast-4.compute.internal>
2026-07-06 21:15:15 +0000

    Fixed free dim tiling

--- benchmarks/operators/leaky_relu/impl_nki.py
+++ benchmarks/operators/leaky_relu/impl_nki.py
@@ -13,24 +13,29 @@ if nki is not None:
     @nki.jit
     def leaky_relu_kernel(a_input):
         num_blocks = (a_input.shape[0] + (PMAX - 1)) // PMAX
+        free_tile_size = 16384
+        num_free_blocks = (a_input.shape[1] + free_tile_size - 1) // free_tile_size
 
         hbm_result_tile = nl.ndarray(a_input.shape, dtype=a_input.dtype, buffer=nl.hbm)
 
         for i in range(num_blocks):
             offset = i * PMAX
-            
             partition_index = nl.arange(PMAX)[:, None]
-            free_dim_index = nl.arange(a_input.shape[1])[None, :]
+            mask_p = partition_index < (a_input.shape[0] - offset)
+            
+            for j in range(num_free_blocks):
+                free_offset = j * free_tile_size
+                free_dim_index = nl.arange(free_tile_size)[None, :]
                 
-            mask = free_dim_index < (a_input.shape[0] - offset)
+                mask_f = free_dim_index < (a_input.shape[1] - free_offset)
+                mask = mask_p & mask_f
 
-            a_tile = nl.load(a_input[offset + partition_index, free_dim_index], mask = mask)
+                a_tile = nl.load(a_input[offset + partition_index, free_offset + free_dim_index], mask = mask)
 
-            scaled_tile = nl.multiply(a_tile, 0.01, mask = mask)
-
-            result_tile = nl.maximum(a_tile, scaled_tile, mask = mask)
-                
-            nl.store(hbm_result_tile[offset + partition_index, free_dim_index], value=result_tile, mask = mask)
+                scaled_tile = nl.multiply(a_tile, 0.01, mask = mask)
+                result_tile = nl.maximum(a_tile, scaled_tile, mask = mask)
+    
+                nl.store(hbm_result_tile[offset + partition_index, free_offset + free_dim_index], value=result_tile, mask = mask)
 
         return hbm_result_tile
 
