dc2f6056f205da9389c35b11a4644cfeed69f07a
Ubuntu <ubuntu@ip-172-31-36-205.ap-southeast-4.compute.internal>
2026-07-07 21:07:00 +0000

    Still needs revision

--- benchmarks/operators/softmax/impl_nki.py
+++ benchmarks/operators/softmax/impl_nki.py
@@ -11,25 +11,32 @@ except ImportError:
 if nki is not None:
     @nki.jit
     def softmax_online_kernel(a_input):
+        free_tile_size = 16384
+
         num_blocks = (a_input.shape[0] + (PMAX - 1)) // PMAX
+        num_free_blocks = (a_input.shape[1] + free_tile_size - 1) // free_tile_size
 
         hbm_result_tile = nl.ndarray(a_input.shape, dtype=a_input.dtype, buffer=nl.hbm)
 
         for i in range(num_blocks):
             offset = i * PMAX
-
             partition_index = nl.arange(PMAX)[:, None]
-            free_dim_index = nl.arange(a_input.shape[1])[None, :]
-            
-            mask = partition_index < (a_input.shape[0] - offset)
+            mask_p = partition_index < (a_input.shape[0] - offset)
 
-            a_tile = nl.load(a_input[offset + partition_index, free_dim_index], mask=mask)
+            for j in range(num_free_blocks):
+                free_offset = j * free_tile_size
+                free_dim_index = nl.arange(free_tile_size)[None, :]
+
+                mask_f = free_dim_index < (a_input.shape[1] - free_offset)
+                mask = mask_p & mask_f
+
+                a_tile = nl.load(a_input[offset + partition_index, free_offset + free_dim_index], mask=mask)
             
-            row_max = nl.max(a_tile, axis=1)
-            shifted = nl.subtract(a_tile, row_max)
-            exp_tile = nl.exp(shifted)
-            row_sum = nl.sum(exp_tile, axis=1)
-            result_tile = nl.divide(exp_tile, row_sum)
+                row_max = nl.max(a_tile, axis=1)
+                shifted = nl.subtract(a_tile, row_max)
+                exp_tile = nl.exp(shifted)
+                row_sum = nl.sum(exp_tile, axis=1)
+                result_tile = nl.divide(exp_tile, row_sum)
             
             nl.store(hbm_result_tile[offset + partition_index, free_dim_index], value=result_tile, mask=mask)
 
