5556a739b9c941d48fd273fda0d37ab30d9983da
Ubuntu <ubuntu@ip-172-31-36-205.ap-southeast-4.compute.internal>
2026-07-07 17:39:47 +0000

    Added free dim tiling

--- benchmarks/operators/sigmoid/impl_nki.py
+++ benchmarks/operators/sigmoid/impl_nki.py
@@ -11,23 +11,28 @@ except ImportError:
 if nki is not None:
     @nki.jit
     def sigmoid_kernel(a_input):
+        free_tile_size = 16384
+
         num_blocks = (a_input.shape[0] + (PMAX - 1)) // PMAX
+        num_free_blocks = (a_input.shape[1] + free_tile_size - 1) // free_tile_size
 
         hbm_result_tile = nl.ndarray(a_input.shape, dtype=a_input.dtype, buffer=nl.hbm)
 
         for i in range(num_blocks):
             offset = i*PMAX
-
             partition_index = nl.arange(PMAX)[:, None]
-            free_dim_index = nl.arange(a_input.shape[1])[None, :]
+            mask_p = partition_index < (a_input.shape[0] - offset)
             
-            mask = partition_index < (a_input.shape[0] - offset)
-
-            a_tile = nl.load(a_input[offset + partition_index, free_dim_index], mask=mask)
-
-            result_tile = nl.sigmoid(a_tile, mask=mask)
+            for j in range(num_free_blocks):
+                free_offset = j * free_tile_size
+                free_dim_index = nl.arange(free_tile_size)[None, :]
+                
+                mask_f = free_dim_index < (a_input.shape[1] - free_offset)
+                mask = mask_p & mask_f
             
-            nl.store(hbm_result_tile[offset + partition_index, free_dim_index], value=result_tile, mask=mask)
+                a_tile = nl.load(a_input[offset + partition_index, free_offset + free_dim_index], mask=mask)
+                result_tile = nl.sigmoid(a_tile, mask=mask)
+                nl.store(hbm_result_tile[offset + partition_index, free_offset + free_dim_index], value=result_tile, mask=mask)
 
         return hbm_result_tile
 
