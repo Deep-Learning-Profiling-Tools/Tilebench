d7d830198b26bc8ba18f2931a038aa9e2e20299c
Ubuntu <ubuntu@ip-172-31-36-205.ap-southeast-4.compute.internal>
2026-07-07 18:02:03 +0000

    Fixed free dim tiling

--- benchmarks/operators/dropout/impl_nki.py
+++ benchmarks/operators/dropout/impl_nki.py
@@ -11,26 +11,32 @@ except ImportError:
 if nki is not None:
     @nki.jit
     def dropout_kernel(x_input, x_keep_input, p):
+        free_tile_size = 16384
+        
         num_blocks = (x_input.shape[0] + (PMAX - 1)) // PMAX
-
+        num_free_blocks = (x_input.shape[1] + free_tile_size - 1) // free_tile_size
+        
         hbm_result_tile = nl.ndarray(x_input.shape, dtype=x_input.dtype, buffer=nl.hbm)
 
         for i in range(num_blocks):
             offset = i*PMAX
-
             partition_index = nl.arange(PMAX)[:, None]
-            free_dim_index = nl.arange(x_input.shape[1])[None, :]
+            mask_p = partition_index < (x_input.shape[0] - offset)
+
+            for j in range(num_free_blocks):
+                free_offset = j * free_tile_size
+                free_dim_index = nl.arange(free_tile_size)[None, :]
             
-            mask = partition_index < (x_input.shape[0] - offset)
+                mask_f = free_dim_index < (x_input.shape[1] - free_offset)
+                mask = mask_p & mask_f
 
-            x_tile = nl.load(x_input[offset + partition_index, free_dim_index], mask=mask)
-            x_keep_tile = nl.load(x_keep_input[offset + partition_index, free_dim_index], mask=mask)
+                x_tile = nl.load(x_input[offset + partition_index, free_offset + free_dim_index], mask=mask)
+                x_keep_tile = nl.load(x_keep_input[offset + partition_index, free_offset + free_dim_index], mask=mask)
 
-            scaled = nl.divide(x_tile, (1.0 - p), mask=mask)
-
-            result_tile = nl.multiply(scaled, x_keep_tile, mask=mask)
+                scaled = nl.divide(x_tile, (1.0 - p), mask=mask)
+                result_tile = nl.multiply(scaled, x_keep_tile, mask=mask)
             
-            nl.store(hbm_result_tile[offset + partition_index, free_dim_index], value=result_tile, mask=mask)
+                nl.store(hbm_result_tile[offset + partition_index, free_offset + free_dim_index], value=result_tile, mask=mask)
 
         return hbm_result_tile
 
