ac8d899e4eccfb0ee5798de60a9bd337d5b02102
Ubuntu <ubuntu@ip-172-31-36-205.ap-southeast-4.compute.internal>
2026-07-07 18:49:38 +0000

    Added free dim tiling

--- benchmarks/operators/swiglu/impl_nki.py
+++ benchmarks/operators/swiglu/impl_nki.py
@@ -11,25 +11,32 @@ except ImportError:
 if nki is not None:
     @nki.jit
     def swiglu_kernel(x_input, y_input):
-        num_blocks = (x_input.shape[0] + PMAX - 1) // PMAX
+        free_tile_size = 16384
+
+        num_blocks = (x_input.shape[0] +(PMAX - 1)) // PMAX
+        num_free_blocks = (x_input.shape[1] + free_tile_size - 1) // free_tile_size
 
         hbm_result_tile = nl.ndarray(x_input.shape, dtype=x_input.dtype, buffer=nl.hbm)
 
         for i in range(num_blocks):
-            offset = i*PMAX
-
+            offset = i * PMAX
             partition_index = nl.arange(PMAX)[:, None]
-            free_dim_index = nl.arange(x_input.shape[1])[None, :]
+            mask_p = partition_index < (x_input.shape[0] - offset)
 
-            mask = partition_index < (x_input.shape[0] - offset)
+            for j in range(num_free_blocks):
+                free_offset = j * free_tile_size
+                free_dim_index = nl.arange(free_tile_size)[None, :]
 
-            x_tile = nl.load(x_input[offset + partition_index, free_dim_index], mask=mask)
-            y_tile = nl.load(y_input[offset + partition_index, free_dim_index], mask=mask)
+                mask_f = free_dim_index < (x_input.shape[1] - free_offset)
+                mask = mask_p & mask_f
 
-            silu_x = nl.silu(x_tile, mask=mask)
-            result_tile = nl.multiply(silu_x, y_tile, mask=mask)
+                x_tile = nl.load(x_input[offset + partition_index, free_offset + free_dim_index], mask=mask)
+                y_tile = nl.load(y_input[offset + partition_index, free_offset + free_dim_index], mask=mask)
 
-            nl.store(hbm_result_tile[offset + partition_index, free_dim_index], value=result_tile, mask=mask)
+                silu_x = nl.silu(x_tile, mask=mask)
+                result_tile = nl.multiply(silu_x, y_tile, mask=mask)
+
+                nl.store(hbm_result_tile[offset + partition_index, free_offset + free_dim_index], value=result_tile, mask=mask)
         
         return hbm_result_tile
 
