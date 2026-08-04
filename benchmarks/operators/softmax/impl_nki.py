8d3081466826ebc5cbcfbd69b09b80f8fd5bb5c9
Ubuntu <ubuntu@ip-172-31-33-19.ap-southeast-4.compute.internal>
2026-08-04 20:43:04 +0000

    modified softmax kernel

--- benchmarks/operators/softmax/impl_nki.py
+++ benchmarks/operators/softmax/impl_nki.py
@@ -5,33 +5,77 @@ try:
     import neuronxcc.nki.language as nl
     import neuronxcc.nki.isa as nisa
     PMAX = nl.tile_size.pmax
+    FREE_CAP = 8192
+    FALLBACK_TILE = 2048
+
 except ImportError:
     nki = None
 
 if nki is not None:
     @nki.jit
     def softmax_online_kernel(a_input):
-        num_blocks = (a_input.shape[0] + (PMAX - 1)) // PMAX
+        n_rows = a_input.shape[0]
+        n_cols = a_input.shape[1]
+        NEG_INF = -3.0e38
 
+        num_blocks = (n_rows + (PMAX - 1)) // PMAX
         hbm_result_tile = nl.ndarray(a_input.shape, dtype=a_input.dtype, buffer=nl.hbm)
 
+        _cap = 2048 if a_input.dtype == nl.float32 else FREE_CAP
+        if n_cols <= _cap:
+            for i in range(num_blocks):
+                offset = i * PMAX
+                partition_index = nl.arange(PMAX)[:, None]
+                free_index = nl.arange(n_cols)[None, :]
+                mask = (partition_index < (n_rows - offset)) & (free_index < n_cols)
+
+                a_tile = nl.load(a_input[offset + partition_index, free_index], mask=mask, dtype=nl.float32)
+                
+                row_max = nl.max(a_tile, axis=1, keepdims=True)
+                exp_tile = nl.exp(nl.subtract(a_tile, row_max))
+                row_sum = nl.sum(exp_tile, axis=1, keepdims=True)
+                
+                result_fp32 = nl.divide(exp_tile, row_sum)
+                result_tile = nl.add(result_fp32, 0.0, dtype=a_input.dtype)
+                nl.store(hbm_result_tile[offset + partition_index, free_index], value=result_tile, mask=mask)
+            return hbm_result_tile
+
+        num_free_blocks = (n_cols + FALLBACK_TILE - 1) // FALLBACK_TILE
         for i in range(num_blocks):
             offset = i * PMAX
-
             partition_index = nl.arange(PMAX)[:, None]
-            free_dim_index = nl.arange(a_input.shape[1])[None, :]
-            
-            mask = partition_index < (a_input.shape[0] - offset)
+            mask_p = partition_index < (n_rows - offset)
 
-            a_tile = nl.load(a_input[offset + partition_index, free_dim_index], mask=mask)
-            
-            row_max = nl.max(a_tile, axis=1)
-            shifted = nl.subtract(a_tile, row_max)
-            exp_tile = nl.exp(shifted)
-            row_sum = nl.sum(exp_tile, axis=1)
-            result_tile = nl.divide(exp_tile, row_sum)
-            
-            nl.store(hbm_result_tile[offset + partition_index, free_dim_index], value=result_tile, mask=mask)
+            row_max = nl.full((PMAX, 1), NEG_INF, dtype=nl.float32, buffer=nl.sbuf)
+            for j in range(num_free_blocks):
+                free_offset = j * FALLBACK_TILE
+                free_dim_index = nl.arange(FALLBACK_TILE)[None, :]
+                mask = mask_p & (free_dim_index < (n_cols - free_offset))
+                a_tile = nl.load(a_input[offset + partition_index, free_offset + free_dim_index], mask=mask, dtype=nl.float32)
+                neg_fill = nl.full(a_tile.shape, NEG_INF, dtype=nl.float32, buffer=nl.sbuf)
+                a_for_max = nl.where(mask, a_tile, neg_fill)
+                row_max[...] = nl.maximum(row_max, nl.max(a_for_max, axis=1, keepdims=True))
+
+            row_sum = nl.zeros((PMAX, 1), dtype=nl.float32, buffer=nl.sbuf)
+            for j in range(num_free_blocks):
+                free_offset = j * FALLBACK_TILE
+                free_dim_index = nl.arange(FALLBACK_TILE)[None, :]
+                mask = mask_p & (free_dim_index < (n_cols - free_offset))
+                a_tile = nl.load(a_input[offset + partition_index, free_offset + free_dim_index], mask=mask, dtype=nl.float32)
+                exp_tile = nl.exp(nl.subtract(a_tile, row_max))
+                zero_tile = nl.zeros(exp_tile.shape, dtype=nl.float32, buffer=nl.sbuf)
+                exp_safe = nl.where(mask, exp_tile, zero_tile)
+                row_sum[...] = nl.add(row_sum, nl.sum(exp_safe, axis=1, keepdims=True))
+
+            for j in range(num_free_blocks):
+                free_offset = j * FALLBACK_TILE
+                free_dim_index = nl.arange(FALLBACK_TILE)[None, :]
+                mask = mask_p & (free_dim_index < (n_cols - free_offset))
+                a_tile = nl.load(a_input[offset + partition_index, free_offset + free_dim_index], mask=mask, dtype=nl.float32)
+                exp_tile = nl.exp(nl.subtract(a_tile, row_max))
+                result_fp32 = nl.divide(exp_tile, row_sum)
+                result_tile = nl.add(result_fp32, 0.0, dtype=a_input.dtype)
+                nl.store(hbm_result_tile[offset + partition_index, free_offset + free_dim_index], value=result_tile, mask=mask)
 
         return hbm_result_tile
 
