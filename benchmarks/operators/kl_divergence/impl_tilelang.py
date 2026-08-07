c847249ddde8b8055bc9e3ab74a6d939051a6252
VolleyDoodle <aarooshr@gmail.com>
2026-08-07 11:47:35 -0700

    delete comments and fix to use T.Select

--- benchmarks/operators/kl_divergence/impl_tilelang.py
+++ benchmarks/operators/kl_divergence/impl_tilelang.py
@@ -6,17 +6,13 @@ from tilelang.autotuner import set_autotune_inputs
 _DEFAULT_CONFIG = {"BLOCK_SIZE": 1024, "threads": 128}
 _last_autotune_config: dict = {}
 
-
-#stages might not matter
 def kl_divergence_config():
     BLOCK_SIZE = [512, 1024, 2048, 4096]
     threads = [64, 128, 256]
-    #stages = [2, 3, 4]
     return [
         dict(BLOCK_SIZE=bs, threads=nt)
         for bs in BLOCK_SIZE
         for nt in threads
-        #for ns in stages
     ]
 @tilelang.autotune(configs=kl_divergence_config(), warmup = 20, rep = 100, timeout = 60)
 @tilelang.jit
@@ -30,8 +26,6 @@ def kl_divergence_kernel(log_y_pred, y_true, loss, dtype,
     with T.Kernel(M, threads=threads) as row:
         y_true_local = T.alloc_fragment((BLOCK_SIZE, ), "float32")
         loss_local_arr = T.alloc_fragment((BLOCK_SIZE, ), "float32")
-        #essentially same as T.alloc_local as it creates one copy per thread
-        #however, becomes invalid buffer scopes as local.fragment and local
         local_log_y_pred = T.alloc_fragment((BLOCK_SIZE, ), "float32")
         loss_local = T.alloc_fragment((1, ), "float32")
         loss_global = T.alloc_fragment((1, ), "float32")
@@ -42,12 +36,12 @@ def kl_divergence_kernel(log_y_pred, y_true, loss, dtype,
             T.copy(log_y_pred[row : row + 1, start:end], local_log_y_pred)
             T.fill(loss_local_arr, 0.0)
             for i in T.Parallel(BLOCK_SIZE):
-                if start + i < N:
-                    yt = y_true_local[i]
-                    if yt > 0.0:
-                        loss_local_arr[i] = yt * (
-                            T.log(yt) - T.Cast("float32", local_log_y_pred[i])
-                        )
+                yt = y_true_local[i]
+                loss_local_arr[i] = T.Select(
+                    yt > 0.0,
+                    yt * (T.log(yt) - T.Cast("float32", local_log_y_pred[i])),
+                    0.0,
+                )
             T.reduce_sum(loss_local_arr, loss_local, dim = 0, clear = True)
             loss_global[0] += loss_local[0]
         T.copy(loss_global, loss[row:row+1])
